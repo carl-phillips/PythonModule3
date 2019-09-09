f3a41cf3590a64b3674967b34542e3eaf51dff16
cfphillips2 <36976465+cfphillips2@users.noreply.github.com>
2019-09-08 21:52:48 -0500

    Create average function, update tests

--- fomat_output/average_scores.py
+++ fomat_output/average_scores.py
@@ -1,2 +1,16 @@
-def average():
-    pass
+def average(score1, score2, score3):
+    return (int(score1) + int(score2) + int(score3)) / 3
+
+
+def main():
+    fname = input("Your first name: ")
+    lname = input("Your last name: ")
+    age = input("Your age: ")
+    score1 = input("First score: ")
+    score2 = input("Second score: ")
+    score3 = input("Third score: ")
+    avg = average(score1, score2, score3)
+    print(fname + ' ' + lname + ', your average score is: ' + str(avg));
+
+
+main()
--- test/test.py
+++ test/test.py
@@ -1,10 +1,11 @@
 import unittest
+from unittest import mock
 from fomat_output import average_scores
 
 class test(unittest.TestCase):
     def test_average(self):
         with mock.patch('builtins.input', side_effect=[85,90,95]):
-            assert average_scores.average() == 90
+            assert average_scores.average(85, 90, 95) == 90
 
 if __name__ == '__main__':
     unittest.main()
